01486056f23650cd5eedea7cf21733b246a6891f
JL <jloo6778@gmail.com>
2024-02-28 12:03:44 -0500

    ok

--- config.php
+++ config.php
@@ -53,6 +53,6 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'caseroproducciondb');
+define('DB_DATABASE', 'bd_micaserodigital');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
